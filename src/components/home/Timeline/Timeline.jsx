9d305bbaba80ad6ded9ec6a9ac1b01bda8a0b6ce
Sumdiboii <spimplikar08@gmail.com>
2025-02-10 23:29:28 +0530

    better distancing of points and smoother flow

--- src/components/home/Timeline/Timeline.jsx
+++ src/components/home/Timeline/Timeline.jsx
@@ -23,15 +23,15 @@ const Timeline = () => {
       <div className="line" style={{ background: `linear-gradient(to bottom, red ${scrollProgress * 100}%, white 0%)` }}></div>
 
       <div className="line" style={{ background: `linear-gradient(to bottom, red ${scrollProgress * 100}%, white 0%)` }}>
-        <div className="point" style={{ top: "7%", backgroundColor: scrollProgress >= 0.07 ? "red" : "white" }}></div>
-        <div className="point" style={{ top: "17%", backgroundColor: scrollProgress >= 0.17 ? "red" : "white" }}></div>
+        <div className="point" style={{ top: "5%", backgroundColor: scrollProgress >= 0.07 ? "red" : "white" }}></div>
+        <div className="point" style={{ top: "16%", backgroundColor: scrollProgress >= 0.17 ? "red" : "white" }}></div>
         <div className="point" style={{ top: "27%", backgroundColor: scrollProgress >= 0.27 ? "red" : "white" }}></div>
-        <div className="point" style={{ top: "37%", backgroundColor: scrollProgress >= 0.37 ? "red" : "white" }}></div>
-        <div className="point" style={{ top: "47%", backgroundColor: scrollProgress >= 0.47 ? "red" : "white" }}></div>
-        <div className="point" style={{ top: "57%", backgroundColor: scrollProgress >= 0.57 ? "red" : "white" }}></div>
-        <div className="point" style={{ top: "67%", backgroundColor: scrollProgress >= 0.67 ? "red" : "white" }}></div>
-        <div className="point" style={{ top: "77%", backgroundColor: scrollProgress >= 0.77 ? "red" : "white" }}></div>
-        <div className="point" style={{ top: "87%", backgroundColor: scrollProgress >= 0.87 ? "red" : "white" }}></div>
+        <div className="point" style={{ top: "39%", backgroundColor: scrollProgress >= 0.37 ? "red" : "white" }}></div>
+        <div className="point" style={{ top: "50%", backgroundColor: scrollProgress >= 0.47 ? "red" : "white" }}></div>
+        <div className="point" style={{ top: "61%", backgroundColor: scrollProgress >= 0.57 ? "red" : "white" }}></div>
+        <div className="point" style={{ top: "72%", backgroundColor: scrollProgress >= 0.67 ? "red" : "white" }}></div>
+        <div className="point" style={{ top: "84%", backgroundColor: scrollProgress >= 0.77 ? "red" : "white" }}></div>
+        <div className="point" style={{ top: "95%", backgroundColor: scrollProgress >= 0.87 ? "red" : "white" }}></div>
       </div>
 
       <div className="line" style={{ background: `linear-gradient(to bottom, red ${scrollProgress * 100}%, white 0%)` }}></div>
