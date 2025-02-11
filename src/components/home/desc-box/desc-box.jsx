9e59bb0316af476a25b7dc41f25607cf9e9064d7
Sumdiboii <spimplikar08@gmail.com>
2025-02-11 20:49:25 +0530

    text box has been added next to the calendar...yet to format it though...have to work on a couple of things...button text, responsiveness on smaller screens, text box formatting and getting event details

--- src/components/home/desc-box/desc-box.jsx
+++ src/components/home/desc-box/desc-box.jsx
@@ -5,6 +5,10 @@ import Calendar from "./calendar/calendar.jsx";
 const DescBox = () => {
   return <div className="desc-box">
     <Calendar />
+
+    <div className="desc-box-info">
+    <p className="glitch" data-text="Mark your calendars! 🚀 Anantya 25 is on the horizon – a tech fest packed with thrilling competitions and epic rewards! Don’t miss out on the action!">Mark your calendars! 🚀 Anantya 25 is on the horizon – a tech fest packed with thrilling competitions and epic rewards! Don’t miss out on the action!</p>
+    </div>
   </div>;
 
   
