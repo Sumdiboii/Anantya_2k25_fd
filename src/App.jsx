0c6f94560c803e39af63ce06ce4726da9e774d40
Sumdiboii <spimplikar08@gmail.com>
2025-02-11 18:47:11 +0530

    added desc-box and plan on adding a calendar and some text in it...responsiveness pending for timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'; // Ensure this CSS contains the global styles
 import Navbar from './components/home/navbar/navbar';
 import Timeline from './components/home/Timeline/Timeline';
+import DescBox from "./components/home/desc-box/desc-box";
 
 
 
@@ -9,8 +10,7 @@ function App() {
     <>
       {/* Navbar Component */}
       <Navbar />
-
-      
+      <DescBox />
 
       {/* Space between Logo-dashboard and Timeline */}
       <div style={{ marginTop: '70px' }}>
---
+++ src/components/home/desc-box/desc-box.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./desc-box.css";
+import Calendar from "./calendar/calendar";
+
+const DescBox = () => {
+  return <div className="desc-box"></div>;
+  <Calendar />
+};
+
+export default DescBox;
