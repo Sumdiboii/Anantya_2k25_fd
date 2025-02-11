5b36073b1c488c6b2cb9bb667b231b12822e11cf
Sumdiboii <spimplikar08@gmail.com>
2025-02-11 20:25:32 +0530

    calender has been added but it not exactly responsive...firstly make the app then care about smaller screen responsiveness.

--- src/components/home/desc-box/desc-box.jsx
+++ src/components/home/desc-box/desc-box.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./desc-box.css";
-import Calendar from "./calendar/calendar";
+import Calendar from "./calendar/calendar.jsx";
 
 const DescBox = () => {
-  return <div className="desc-box"></div>;
-  <Calendar />
+  return <div className="desc-box">
+    <Calendar />
+  </div>;
+
+  
 };
 
 export default DescBox;
